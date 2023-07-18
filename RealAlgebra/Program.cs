9297c0e8816ba35e531601d34720677a0568729c
gaurav061120 <gaurav.mishra0611@gmail.com>
2023-07-18 12:59:42 +0530

    initial commit

---
+++ RealAlgebra/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShapeAreaLib;
+namespace RealAlgebra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Please select shape");
+                Console.WriteLine("1. Rectangle");
+                Console.WriteLine("2. Square");
+                Console.WriteLine("3. Tiangle");
+                Console.WriteLine("0. Exit");
+                Console.Write("enter choice");
+                string input = Console.ReadLine();
+                int choice;
+                if (int.TryParse(input, out choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            CalculateRectangleArea();
+                            break;
+                        case 2:
+                            CalculateSquareArea();
+                            break;
+                        case 3:
+                            CalculateTriangleArea();
+                            break;
+                        case 0:
+                            Console.WriteLine("thanx for using RealAlgebra");
+                            return;
+                        default:
+                            Console.WriteLine("inalid input");
+                            break;
+                    }
+                }
+                void CalculateRectangleArea()
+                {
+                    Console.Write("enter length of rectangle");
+                    double length = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("enter width of rectangle");
+                    double width = Convert.ToDouble(Console.ReadLine());
+                    double Area = ShapeAreaLib.Rectangle.CalculateArea(length, width);
+                    Console.WriteLine($"the area of rectangle is : {Area}");
+                }
+                void CalculateSquareArea()
+                {
+                    Console.Write("enter side of square");
+                    double side = Convert.ToDouble(Console.ReadLine());
+
+
+                    double Area = ShapeAreaLib.Square.CalculateArea(side);
+                    Console.WriteLine($"the area of square is : {Area}");
+                }
+                void CalculateTriangleArea()
+                {
+                    Console.Write("enter baseLength of triangle");
+                    double baseLength = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("enter heigth of rectangle");
+                    double height = Convert.ToDouble(Console.ReadLine());
+                    double Area = ShapeAreaLib.Triangle.CalculateArea(baseLength, height);
+                    Console.WriteLine($"the area of triangle is : {Area}");
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+                
+
+
+
+            
+        
+    
+
---
+++ ShapeAreaLib/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeAreaLib
+{
+
+    
+    
+        public class Rectangle
+        {
+            public static double CalculateArea(double length, double width)
+            {
+                return length * width;
+
+            }
+        }
+
+        public class Square
+        {
+            public static double CalculateArea(double side)
+            {
+                return side * side;
+
+            }
+        }
+
+        public class Triangle
+        {
+            public static double CalculateArea(double baseLength, double height)
+            {
+                return 0.5 * baseLength * height;
+
+            }
+        }
+    }
+
+
+                
+
